4aec93d2296de9271629f078643b6a638f16c117
Tolkinxon <soliyevtolkinxon@gmail.com>
2023-01-31 01:20:17 +0500

    added useHTTp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react'
 
 
 ReactDOM.render(
---
+++ src/redux/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    news: [],
+    newsLoadingStatus: 'tolkinxon',
+    filters: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch({ type, payload }){
+        case '' : 
+            return
+        case '' : 
+            return
+        default:
+            return state
+    }
+}
